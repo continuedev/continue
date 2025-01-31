Granite-Code/continue-for-granite

Import Paver code as setupGranite
Import the Paver code for setting up Ollama and the Granite Models
into the extension without adaption other than to get it to build
and run within the Continue plugin. The granite.setup command will
open a Webview with the Continue GUI.

extensions/vscode/src/setupGranite/modelServer.ts
@@ -1,0 +1,19 @@
+import { ProgressData } from "core/setupGranite/commons/progressData";
+import { ModelStatus, ServerStatus } from "core/setupGranite/commons/statuses";
+
+export interface IModelServer {
+  getName(): string;
+  getStatus(): Promise<ServerStatus>;
+  startServer(): Promise<boolean>;
+  installServer(mode: string): Promise<boolean>;
+  getModelStatus(modelName?: string): Promise<ModelStatus>
+  installModel(modelName: string, reportProgress: (progress: ProgressData) => void): Promise<any>;
+  supportedInstallModes(): Promise<{ id: string; label: string, supportsRefresh: boolean }[]>; //manual, script, homebrew
+  configureAssistant(
+    chatModel: string | null,
+    tabModel: string | null,
+    embeddingsModel: string | null
+  ): Promise<void>;
+  listModels(): Promise<string[]>;
+  //getModelInfo(modelName: string): Promise<ModelInfo | undefined>;
+}

extensions/vscode/src/setupGranite/utils/sysUtils.ts
@@ -1,0 +1,60 @@
+import * as os from 'os';
+import * as si from 'systeminformation';
+import { DiskSpace, SystemInfo } from 'core/setupGranite/commons/sysInfo';
+
+// Returns the system's total and available memory
+export function getSystemMemory() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  return { totalMemory, freeMemory };
+}
+
+// Returns the system's total number of CPUs
+export function getSystemCPUs() {
+  return os.cpus().length;
+}
+
+export async function getSystemDiskSpace(): Promise<DiskSpace> {
+  try {
+    const fsSize = await si.fsSize();
+    const mainVolume = fsSize.find(drive =>
+      process.platform === 'win32'
+        ? drive.mount.toLowerCase().includes('c:')
+        : drive.mount === '/'
+    );
+    if (mainVolume) {
+      return {
+        mount: mainVolume.mount,
+        totalDiskSpace: mainVolume.size,
+        freeDiskSpace: mainVolume.available
+      };
+    }
+    console.error('Main volume not found');
+  } catch (error) {
+    console.error('Error getting disk space:', error);
+  }
+  return { mount: "Unknown", totalDiskSpace: 0, freeDiskSpace: 0 };
+}
+
+// Returns the system's available GPUs
+export async function getSystemGPUs(): Promise<{ model: string, cores: number }[]> {
+  try {
+    const gpus = await si.graphics();
+    return gpus.controllers.map(controller => ({
+      model: controller.model,
+      cores: controller.cores || 0
+    }));
+  } catch (error) {
+    console.error('Error detecting GPUs:', error);
+    return [];
+  }
+}
+
+export async function getSystemInfo(): Promise<SystemInfo> {
+  return {
+    cpus: getSystemCPUs(),
+    gpus: await getSystemGPUs(),
+    diskSpace: await getSystemDiskSpace(),
+    memory: getSystemMemory()
+  };
+}

gui/src/setupGranite/ModelList.tsx
@@ -1,0 +1,165 @@
+import React from 'react';
+import Select from 'react-select';
+import { ProgressData } from 'core/setupGranite/commons/progressData';
+import ProgressBar from './ProgressBar';
+import { StatusCheck, StatusValue } from './StatusCheck';
+import { ModelStatus } from 'core/setupGranite/commons/statuses';
+import { checkModelCompatibility } from 'core/setupGranite/commons/modelRequirements';
+import ModelWarning from './ModelWarning';
+import { SystemInfo } from 'core/setupGranite/commons/sysInfo';
+
+export interface ModelOption {
+    label: string;
+    value: string | null;
+    info: string | null;
+}
+
+interface ModelListProps {
+    className: string;
+    label: string;
+    value: string | null;
+    onChange: (option: ModelOption | null) => void;
+    status: ModelStatus | null;
+    options: ModelOption[];
+    progress?: ProgressData;
+    tooltip?: string;
+    disabled?: boolean;
+    systemInfo: SystemInfo | null;
+}
+
+const ModelList: React.FC<ModelListProps> = ({ className, label, value, onChange, status, options, progress, tooltip, disabled, systemInfo }) => {
+    const selectHeight = '16px';
+    const customStyles = {
+        container: (base: any) => ({
+            ...base,
+        }),
+        control: (base: any) => ({
+            ...base,
+            minHeight: selectHeight,
+        }),
+        dropdownIndicator: (base: any) => ({
+            ...base,
+            paddingTop: 0,
+            paddingBottom: 0,
+        }),
+        menuList: (base: any) => ({
+            ...base,
+            fontSize: '12px',
+        }),
+        option: (base: any, state: { isSelected: boolean; isFocused: boolean }) => ({
+            ...base,
+            backgroundColor: state.isSelected
+                ? 'var(--vscode-list-activeSelectionBackground)'
+                : state.isFocused
+                    ? 'var(--vscode-list-hoverBackground)'
+                    : 'var(--vscode-dropdown-background)',
+            color: state.isSelected
+                ? 'var(--vscode-list-activeSelectionForeground)'
+                : 'var(--vscode-dropdown-foreground)',
+            ':active': {
+                backgroundColor: 'var(--vscode-list-activeSelectionBackground)',
+                color: 'var(--vscode-list-activeSelectionForeground)',
+            },
+        }),
+        menu: (base: any) => ({
+            ...base,
+            backgroundColor: 'var(--vscode-dropdown-background)',
+        }),
+    };
+
+    const formatOptionLabel = (modelOption: ModelOption, { context }: { context: 'menu' | 'value' }) => {
+        const isSelected = value === modelOption.value;
+        const color = isSelected && context === 'menu' ? 'var(--vscode-quickInputList-focusForeground)' : 'var(--vscode-menu-foreground)';
+        const style = {
+            display: 'flex',
+            width: context === 'menu' ? '250px' : '210px',
+            justifyContent: 'space-between',
+        };
+        return (
+            <div style={style}>
+                <span style={{ color }}>{modelOption.label}</span>
+                <span className='model-option--info' style={{ color }}>{modelOption.info}</span>
+            </div>
+        );
+    };
+
+    const getIconType = (status: ModelStatus | null): StatusValue | null => {
+        if (status === null) {
+            return null;
+        }
+        switch (status) {
+            case ModelStatus.installed:
+                return 'complete';
+            case ModelStatus.stale:
+                return 'partial';
+            case ModelStatus.installing:
+                return 'installing';
+        }
+        return 'missing';
+    };
+
+    const getTitle = (status: ModelStatus | null): string | undefined => {
+        switch (status) {
+            case ModelStatus.stale:
+                return 'There is a newer version of this model';
+            case ModelStatus.installed:
+                return 'This model is already installed';
+            case ModelStatus.installing:
+                return 'This model is being installed';
+            case ModelStatus.missing:
+                return 'This model will be installed';
+        }
+    };
+
+    const getStatusLabel = (status: ModelStatus): string => {
+        switch (status) {
+            case ModelStatus.stale:
+                return '(will be updated automatically)';
+            case ModelStatus.missing:
+                return '(will be pulled automatically)';
+            default:
+                return '';
+        }
+    };
+
+    const { warnings, errors } = checkModelCompatibility(value, systemInfo);
+
+    return (
+        <div className="form-group">
+            <div className='model-list--outer-wrapper'>
+
+                <label className='model-list--label' htmlFor={label}>
+                    <StatusCheck type={getIconType(status)} title={getTitle(status)} />
+                    <span title={tooltip}>{label}:</span>
+                </label>
+
+                <div className={className + `--wrapper`}>
+                    <Select
+                        className={className}
+                        id={label}
+                        value={options.find(option => option.value === value)}
+                        onChange={(newValue) => onChange(newValue as ModelOption)}
+                        options={options}
+                        isDisabled={disabled}
+                        styles={customStyles}
+                        formatOptionLabel={formatOptionLabel}
+                        isSearchable={false} // Disable typing in dropdown
+                    />
+                    {status !== null && status !== ModelStatus.installed && !progress && <span className='info-label' style={{ display: 'flex', alignItems: 'center' }}> {getStatusLabel(status)}</span>}
+                </div >
+            </div >
+
+            <ModelWarning warnings={warnings} errors={errors} />
+
+            <div className='progress-container'>
+                {progress && (
+                    <div id='progress' style={{ width: "100%", marginTop: "8px" }}>
+                        <ProgressBar id={value!} data={progress} />
+                    </div>
+                )}
+            </div>
+        </div >
+    );
+};
+
+export default ModelList;

gui/src/setupGranite/ProgressBar.tsx
@@ -1,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { ProgressData } from "core/setupGranite/commons/progressData";
+import { formatSize, formatTime } from 'core/setupGranite/commons/textUtils';
+import { MB } from "core/setupGranite/commons/sizeUtils";
+
+// The ProgressBar component
+const ProgressBar: React.FC<{ data: ProgressData; id: string }> = ({ data, id }) => {
+  const [speed, setSpeed] = useState(0); // MB/s
+  const [, setElapsedTime] = useState(0); // Time since start in seconds
+  const [previousCompleted, setPreviousCompleted] = useState(data.completed); // Store previous completed value to calculate speed
+  const [lastUpdateTime, setLastUpdateTime] = useState(Date.now());
+  const [speedHistory, setSpeedHistory] = useState<number[]>([]);
+
+  const SPEED_HISTORY_LENGTH = 50; // Number of data points to use for smoothing
+
+  const [status, setStatus] = useState("");
+  const [completedSize, setCompletedSize] = useState("0 MB");
+  const [totalSize, setTotalSize] = useState("0 MB");
+  const [progressPercentage, setProgressPercentage] = useState(0);
+  const [estimatedCompletion, setEstimatedCompletion] = useState("0s");
+
+  useEffect(() => {
+    if (data.key !== id) {
+      return;
+    }
+    const now = Date.now();
+    const timeDiff = (now - lastUpdateTime) / 1000; // Convert to seconds
+
+    // Calculate speed in MB/s
+    const completedMB = data.completed !== undefined ? data.completed / MB : 0;
+    const prevCompletedMB = previousCompleted !== undefined ? previousCompleted / MB : 0;
+    const deltaMB = Math.max(0, completedMB - prevCompletedMB); // Ensure non-negative delta
+    setStatus(data.status);
+    // Update speed (MB/s)
+    const newSpeed = timeDiff > 0 ? deltaMB / timeDiff : 0;
+
+    // Update speed history
+    setSpeedHistory(prevHistory => {
+      const updatedHistory = [...prevHistory, newSpeed].slice(-SPEED_HISTORY_LENGTH);
+      const averageSpeed = updatedHistory.reduce((sum, speed) => sum + speed, 0) / updatedHistory.length;
+      setSpeed(Math.max(0, averageSpeed)); // Ensure non-negative average speed
+      return updatedHistory;
+    });
+
+    setElapsedTime((prev) => prev + timeDiff);
+    setPreviousCompleted(data.completed);
+    setLastUpdateTime(now);
+
+    // Update size and progress information
+    setCompletedSize(formatSize(data.completed || 0, 2));
+    setTotalSize(formatSize(data.total || 0, 2));
+    setProgressPercentage(data.completed !== undefined && data.total !== undefined ? (data.completed / data.total) * 100 : 0);
+
+    const remainingBits = data.total !== undefined && data.completed !== undefined ? data.total - data.completed : 0;
+    const remainingSeconds = remainingBits > 0 && speed > 0 ? remainingBits / (speed * MB) : 0;
+    setEstimatedCompletion(formatTime(remainingSeconds));
+  }, [data.status, data.completed]);
+
+
+
+  useEffect(() => {
+    // Reset all state for new download
+    setStatus("");
+    setSpeed(0);
+    setElapsedTime(0);
+    setPreviousCompleted(0);
+    setLastUpdateTime(0);
+    setSpeedHistory([]);
+    // Reset new state variables
+    setCompletedSize("");
+    setTotalSize("");
+    setProgressPercentage(0);
+    setEstimatedCompletion("Universe heat death");
+  }, [id]);
+
+  if (!data || data.key !== id || data.status?.toLowerCase() === 'success') {
+    return <></>;
+  }
+
+  return (
+    <div style={{ margin: '5px' }}>
+      <label className="progress-status">
+        {status}
+        <div style={{ fontSize: '12px', marginLeft: 'auto' }}>
+          {data.completed! < data.total! && `est. time: ~${estimatedCompletion}`}
+        </div>
+      </label>
+      <div style={{ display: 'flex', alignItems: 'flex-start', marginBottom: '10px', marginTop: '5px' }}>
+        <div style={{
+          flex: '0 0 100%',
+          marginRight: '10px'
+        }} >
+          <div style={{
+            height: "8px",
+            backgroundColor: "#e0e0df",
+            borderRadius: "5px",
+          }}>
+            <div className="progress-bar"
+              style={{
+                width: `${progressPercentage}%`,
+                height: '100%',
+                borderRadius: '5px'
+              }}
+            />
+          </div>
+          {data.total && (
+            <div style={{ textAlign: 'right', fontSize: '10px', margin: '5px 0' }}>
+              {formatSize(data.completed || 0, 2).padStart(10)} / {formatSize(data.total, 2).padStart(10)} at {speed.toFixed(2).padStart(7)} MB/s
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;

continuedev/continue

extensions/vscode/src/granite/modelServer.ts
@@ -1,0 +1,19 @@
+import { ProgressData } from "core/granite/commons/progressData";
+import { ModelStatus, ServerStatus } from "core/granite/commons/statuses";
+
+export interface IModelServer {
+  getName(): string;
+  getStatus(): Promise<ServerStatus>;
+  startServer(): Promise<boolean>;
+  installServer(mode: string): Promise<boolean>;
+  getModelStatus(modelName?: string): Promise<ModelStatus>
+  installModel(modelName: string, reportProgress: (progress: ProgressData) => void): Promise<any>;
+  supportedInstallModes(): Promise<{ id: string; label: string, supportsRefresh: boolean }[]>; //manual, script, homebrew
+  configureAssistant(
+    chatModel: string | null,
+    tabModel: string | null,
+    embeddingsModel: string | null
+  ): Promise<void>;
+  listModels(): Promise<string[]>;
+  //getModelInfo(modelName: string): Promise<ModelInfo | undefined>;
+}

extensions/vscode/src/granite/utils/sysUtils.ts
@@ -1,0 +1,60 @@
+import * as os from 'os';
+import * as si from 'systeminformation';
+import { DiskSpace, SystemInfo } from 'core/granite/commons/sysInfo';
+
+// Returns the system's total and available memory
+export function getSystemMemory() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  return { totalMemory, freeMemory };
+}
+
+// Returns the system's total number of CPUs
+export function getSystemCPUs() {
+  return os.cpus().length;
+}
+
+export async function getSystemDiskSpace(): Promise<DiskSpace> {
+  try {
+    const fsSize = await si.fsSize();
+    const mainVolume = fsSize.find(drive =>
+      process.platform === 'win32'
+        ? drive.mount.toLowerCase().includes('c:')
+        : drive.mount === '/'
+    );
+    if (mainVolume) {
+      return {
+        mount: mainVolume.mount,
+        totalDiskSpace: mainVolume.size,
+        freeDiskSpace: mainVolume.available
+      };
+    }
+    console.error('Main volume not found');
+  } catch (error) {
+    console.error('Error getting disk space:', error);
+  }
+  return { mount: "Unknown", totalDiskSpace: 0, freeDiskSpace: 0 };
+}
+
+// Returns the system's available GPUs
+export async function getSystemGPUs(): Promise<{ model: string, cores: number }[]> {
+  try {
+    const gpus = await si.graphics();
+    return gpus.controllers.map(controller => ({
+      model: controller.model,
+      cores: controller.cores || 0
+    }));
+  } catch (error) {
+    console.error('Error detecting GPUs:', error);
+    return [];
+  }
+}
+
+export async function getSystemInfo(): Promise<SystemInfo> {
+  return {
+    cpus: getSystemCPUs(),
+    gpus: await getSystemGPUs(),
+    diskSpace: await getSystemDiskSpace(),
+    memory: getSystemMemory()
+  };
+}

gui/src/granite/ModelList.tsx
@@ -1,0 +1,165 @@
+import React from 'react';
+import Select from 'react-select';
+import { ProgressData } from 'core/granite/commons/progressData';
+import ProgressBar from './ProgressBar';
+import { StatusCheck, StatusValue } from './StatusCheck';
+import { ModelStatus } from 'core/granite/commons/statuses';
+import { checkModelCompatibility } from 'core/granite/commons/modelRequirements';
+import ModelWarning from './ModelWarning';
+import { SystemInfo } from 'core/granite/commons/sysInfo';
+
+export interface ModelOption {
+    label: string;
+    value: string | null;
+    info: string | null;
+}
+
+interface ModelListProps {
+    className: string;
+    label: string;
+    value: string | null;
+    onChange: (option: ModelOption | null) => void;
+    status: ModelStatus | null;
+    options: ModelOption[];
+    progress?: ProgressData;
+    tooltip?: string;
+    disabled?: boolean;
+    systemInfo: SystemInfo | null;
+}
+
+const ModelList: React.FC<ModelListProps> = ({ className, label, value, onChange, status, options, progress, tooltip, disabled, systemInfo }) => {
+    const selectHeight = '16px';
+    const customStyles = {
+        container: (base: any) => ({
+            ...base,
+        }),
+        control: (base: any) => ({
+            ...base,
+            minHeight: selectHeight,
+        }),
+        dropdownIndicator: (base: any) => ({
+            ...base,
+            paddingTop: 0,
+            paddingBottom: 0,
+        }),
+        menuList: (base: any) => ({
+            ...base,
+            fontSize: '12px',
+        }),
+        option: (base: any, state: { isSelected: boolean; isFocused: boolean }) => ({
+            ...base,
+            backgroundColor: state.isSelected
+                ? 'var(--vscode-list-activeSelectionBackground)'
+                : state.isFocused
+                    ? 'var(--vscode-list-hoverBackground)'
+                    : 'var(--vscode-dropdown-background)',
+            color: state.isSelected
+                ? 'var(--vscode-list-activeSelectionForeground)'
+                : 'var(--vscode-dropdown-foreground)',
+            ':active': {
+                backgroundColor: 'var(--vscode-list-activeSelectionBackground)',
+                color: 'var(--vscode-list-activeSelectionForeground)',
+            },
+        }),
+        menu: (base: any) => ({
+            ...base,
+            backgroundColor: 'var(--vscode-dropdown-background)',
+        }),
+    };
+
+    const formatOptionLabel = (modelOption: ModelOption, { context }: { context: 'menu' | 'value' }) => {
+        const isSelected = value === modelOption.value;
+        const color = isSelected && context === 'menu' ? 'var(--vscode-quickInputList-focusForeground)' : 'var(--vscode-menu-foreground)';
+        const style = {
+            display: 'flex',
+            width: context === 'menu' ? '250px' : '210px',
+            justifyContent: 'space-between',
+        };
+        return (
+            <div style={style}>
+                <span style={{ color }}>{modelOption.label}</span>
+                <span className='model-option--info' style={{ color }}>{modelOption.info}</span>
+            </div>
+        );
+    };
+
+    const getIconType = (status: ModelStatus | null): StatusValue | null => {
+        if (status === null) {
+            return null;
+        }
+        switch (status) {
+            case ModelStatus.installed:
+                return 'complete';
+            case ModelStatus.stale:
+                return 'partial';
+            case ModelStatus.installing:
+                return 'installing';
+        }
+        return 'missing';
+    };
+
+    const getTitle = (status: ModelStatus | null): string | undefined => {
+        switch (status) {
+            case ModelStatus.stale:
+                return 'There is a newer version of this model';
+            case ModelStatus.installed:
+                return 'This model is already installed';
+            case ModelStatus.installing:
+                return 'This model is being installed';
+            case ModelStatus.missing:
+                return 'This model will be installed';
+        }
+    };
+
+    const getStatusLabel = (status: ModelStatus): string => {
+        switch (status) {
+            case ModelStatus.stale:
+                return '(will be updated automatically)';
+            case ModelStatus.missing:
+                return '(will be pulled automatically)';
+            default:
+                return '';
+        }
+    };
+
+    const { warnings, errors } = checkModelCompatibility(value, systemInfo);
+
+    return (
+        <div className="form-group">
+            <div className='model-list--outer-wrapper'>
+
+                <label className='model-list--label' htmlFor={label}>
+                    <StatusCheck type={getIconType(status)} title={getTitle(status)} />
+                    <span title={tooltip}>{label}:</span>
+                </label>
+
+                <div className={className + `--wrapper`}>
+                    <Select
+                        className={className}
+                        id={label}
+                        value={options.find(option => option.value === value)}
+                        onChange={(newValue) => onChange(newValue as ModelOption)}
+                        options={options}
+                        isDisabled={disabled}
+                        styles={customStyles}
+                        formatOptionLabel={formatOptionLabel}
+                        isSearchable={false} // Disable typing in dropdown
+                    />
+                    {status !== null && status !== ModelStatus.installed && !progress && <span className='info-label' style={{ display: 'flex', alignItems: 'center' }}> {getStatusLabel(status)}</span>}
+                </div >
+            </div >
+
+            <ModelWarning warnings={warnings} errors={errors} />
+
+            <div className='progress-container'>
+                {progress && (
+                    <div id='progress' style={{ width: "100%", marginTop: "8px" }}>
+                        <ProgressBar id={value!} data={progress} />
+                    </div>
+                )}
+            </div>
+        </div >
+    );
+};
+
+export default ModelList;

gui/src/granite/ProgressBar.tsx
@@ -1,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { ProgressData } from "core/granite/commons/progressData";
+import { formatSize, formatTime } from 'core/granite/commons/textUtils';
+import { MB } from "core/granite/commons/sizeUtils";
+
+// The ProgressBar component
+const ProgressBar: React.FC<{ data: ProgressData; id: string }> = ({ data, id }) => {
+  const [speed, setSpeed] = useState(0); // MB/s
+  const [, setElapsedTime] = useState(0); // Time since start in seconds
+  const [previousCompleted, setPreviousCompleted] = useState(data.completed); // Store previous completed value to calculate speed
+  const [lastUpdateTime, setLastUpdateTime] = useState(Date.now());
+  const [speedHistory, setSpeedHistory] = useState<number[]>([]);
+
+  const SPEED_HISTORY_LENGTH = 50; // Number of data points to use for smoothing
+
+  const [status, setStatus] = useState("");
+  const [completedSize, setCompletedSize] = useState("0 MB");
+  const [totalSize, setTotalSize] = useState("0 MB");
+  const [progressPercentage, setProgressPercentage] = useState(0);
+  const [estimatedCompletion, setEstimatedCompletion] = useState("0s");
+
+  useEffect(() => {
+    if (data.key !== id) {
+      return;
+    }
+    const now = Date.now();
+    const timeDiff = (now - lastUpdateTime) / 1000; // Convert to seconds
+
+    // Calculate speed in MB/s
+    const completedMB = data.completed !== undefined ? data.completed / MB : 0;
+    const prevCompletedMB = previousCompleted !== undefined ? previousCompleted / MB : 0;
+    const deltaMB = Math.max(0, completedMB - prevCompletedMB); // Ensure non-negative delta
+    setStatus(data.status);
+    // Update speed (MB/s)
+    const newSpeed = timeDiff > 0 ? deltaMB / timeDiff : 0;
+
+    // Update speed history
+    setSpeedHistory(prevHistory => {
+      const updatedHistory = [...prevHistory, newSpeed].slice(-SPEED_HISTORY_LENGTH);
+      const averageSpeed = updatedHistory.reduce((sum, speed) => sum + speed, 0) / updatedHistory.length;
+      setSpeed(Math.max(0, averageSpeed)); // Ensure non-negative average speed
+      return updatedHistory;
+    });
+
+    setElapsedTime((prev) => prev + timeDiff);
+    setPreviousCompleted(data.completed);
+    setLastUpdateTime(now);
+
+    // Update size and progress information
+    setCompletedSize(formatSize(data.completed || 0, 2));
+    setTotalSize(formatSize(data.total || 0, 2));
+    setProgressPercentage(data.completed !== undefined && data.total !== undefined ? (data.completed / data.total) * 100 : 0);
+
+    const remainingBits = data.total !== undefined && data.completed !== undefined ? data.total - data.completed : 0;
+    const remainingSeconds = remainingBits > 0 && speed > 0 ? remainingBits / (speed * MB) : 0;
+    setEstimatedCompletion(formatTime(remainingSeconds));
+  }, [data.status, data.completed]);
+
+
+
+  useEffect(() => {
+    // Reset all state for new download
+    setStatus("");
+    setSpeed(0);
+    setElapsedTime(0);
+    setPreviousCompleted(0);
+    setLastUpdateTime(0);
+    setSpeedHistory([]);
+    // Reset new state variables
+    setCompletedSize("");
+    setTotalSize("");
+    setProgressPercentage(0);
+    setEstimatedCompletion("Universe heat death");
+  }, [id]);
+
+  if (!data || data.key !== id || data.status?.toLowerCase() === 'success') {
+    return <></>;
+  }
+
+  return (
+    <div style={{ margin: '5px' }}>
+      <label className="progress-status">
+        {status}
+        <div style={{ fontSize: '12px', marginLeft: 'auto' }}>
+          {data.completed! < data.total! && `est. time: ~${estimatedCompletion}`}
+        </div>
+      </label>
+      <div style={{ display: 'flex', alignItems: 'flex-start', marginBottom: '10px', marginTop: '5px' }}>
+        <div style={{
+          flex: '0 0 100%',
+          marginRight: '10px'
+        }} >
+          <div style={{
+            height: "8px",
+            backgroundColor: "#e0e0df",
+            borderRadius: "5px",
+          }}>
+            <div className="progress-bar"
+              style={{
+                width: `${progressPercentage}%`,
+                height: '100%',
+                borderRadius: '5px'
+              }}
+            />
+          </div>
+          {data.total && (
+            <div style={{ textAlign: 'right', fontSize: '10px', margin: '5px 0' }}>
+              {formatSize(data.completed || 0, 2).padStart(10)} / {formatSize(data.total, 2).padStart(10)} at {speed.toFixed(2).padStart(7)} MB/s
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;